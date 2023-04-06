3bf214fa5d300dab7e883698a0c30f3a8f1b28a0
Gabriel Moraru <gmoraru04@gmail.com>
2023-04-06 18:54:38 +0300

    operatori

---
+++ Week1/operatori.py
@@ -0,0 +1,18 @@
+print("Hello")
+print('Hello')
+print(3, type(3))
+print(3.0, type(3.0))
+print(3 + 1j, type(3+1j))
+
+print(1 + 2)
+print(1 - 2)
+print(1 * 2)
+print(1 / 2)
+print(1 % 2)
+print(1 // 2)
+print(1 ** 2)
+
+print(1 == 2)
+print(1 == 1)
+print(1 != 2)
+print(1 > 2)
